54dcc5f2baf8859ed894fe91e9369129226bbd2f
Alban Linard <alban@linard.fr>
2015-11-30 16:24:26 +0100

    Add translations to paquito.

---
+++ src/paquito/i18n/en.lua
@@ -0,0 +1,14 @@
+return {
+  en = {
+    ["paquito:description"     ] = "the really easy packager",
+    ["paquito:command:default" ] = "write default configuration",
+    ["paquito:command:load"    ] = "load configuration",
+    ["paquito:command:build"   ] = "guess build dependencies",
+    ["paquito:command:install" ] = "guess installed files",
+    ["paquito:command:run"     ] = "guess runtime dependencies",
+    ["paquito:flag:verbose"    ] = "enable verbose output",
+    ["paquito:flag:quiet"      ] = "disable all output",
+    ["paquito:option:locale"   ] = "locale to use",
+    ["paquito:option:path"     ] = "path to paquito.lua directory",
+  }
+}
