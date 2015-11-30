679a671e1fa7125ad4a1cab98e2d678829865f57
Alban Linard <alban@linard.fr>
2015-11-30 16:24:13 +0100

    Improve paquito launcher by using argparse.

--- src/paquito/init.lua
+++ src/paquito/init.lua
@@ -1,38 +1,89 @@
-#! /usr/bin/env luajit
+local Argparse = require "argparse"
+local Coronest = require "coroutine.make"
+local File     = require "paquito.file"
+local I18n     = require "i18n"
+local Lustache = require "lustache"
+local Serpent  = require "serpent"
 
-local Cli           = require "cliargs"
-Cli:set_name (_G.arg [0])
-Cli:optarg ("project", "url or path", os.getenv "PWD", 1)
-local arguments = Cli:parse (_G.arg)
-if not arguments then
-  os.exit (1)
-end
-
-_G.coroutine        = require "coroutine.make" ()
-local Copas         = require "copas"
-local Lustache      = require "lustache"
-local Yaml          = require "yaml"
-local Main          = require "paquito.main"
-
-Yaml.configure {
-  load_nulls_as_nil = true,
-  sort_table_keys   = true,
-}
+_G.coroutine = Coronest ()
 
 do
-  local metatable = getmetatable ""
-  metatable.__mod = function (pattern, variables)
+  local Metatable = getmetatable ""
+  Metatable.__mod = function (pattern, variables)
     return Lustache:render (pattern, variables)
-  end
-  -- http://lua-users.org/wiki/StringTrim
-  function _G.string.trim (s)
-    return s:match "^()%s*$" and "" or s:match "^%s*(.*%S)"
   end
 end
 
-Copas.addthread (function ()
-  local main = Main.new ()
-  main (arguments)
-end)
+local function show (t)
+  return Serpent.line (t, {
+    comment  = false,
+    compact  = false,
+    fatal    = true,
+    indent   = "  ",
+    sortkeys = true,
+    sparse   = false,
+  })
+end
 
-Copas.loop ()
+local locale = (os.getenv "LANG" or "en")
+             : match "[^%.]+"
+             : gsub ("_", "-")
+
+do
+  I18n.load (require "paquito.i18n.en")
+  local ok, translation = pcall (require, "paquito.i18n.{{{locale}}}" % {
+    locale = locale,
+  })
+  if ok then
+    I18n.load (translation)
+  end
+end
+
+local parser   = Argparse () {
+  name        = "paquito",
+  description = I18n "paquito:description",
+}
+parser:require_command (true)
+parser:option "-l" "--locale" {
+  description = I18n "paquito:option:locale",
+  default     = locale,
+}
+parser:mutex (
+  parser:flag "-q" "--quiet" {
+    description = I18n "paquito:flag:quiet",
+  },
+  parser:flag "-v" "--verbose" {
+    description = I18n "paquito:flag:verbose",
+  }
+)
+parser:option "-p" "--path" {
+  description = I18n "paquito:option:path",
+  default     = os.getenv "PWD",
+}
+
+local state = {
+  configuration = File.decode (os.getenv "HOME" .. "/.paquito.conf")
+               or {},
+  data          = nil,
+}
+
+local modules = {
+  require "paquito.command.default" (state),
+  require "paquito.command.load"    (state),
+--  require "paquito.command.build"   (),
+--  require "paquito.command.install" (),
+--  require "paquito.command.run"     (),
+}
+
+for i = 1, #modules do
+  modules [i] (parser)
+end
+
+state.arguments = parser:parse (_G.arg)
+print ("arguments", show (state.arguments))
+
+print ("state", show (state))
+for i = 1, #modules do
+  modules [i] (state.arguments)
+end
+print ("state", show (state))
